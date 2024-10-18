1ac216f83935e57572bf550f888fff476e19d208
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-18 19:52:33 +0200

    Use agent in App

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -5,10 +5,10 @@ axios.defaults.baseURL = "http://localhost:5000/api";
 const responseBody = (response: AxiosResponse) => response.data;
 
 const requests = {
-  get: (url: string) => axios.get(url).then(responseBody),
-  post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
-  put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
-  delete: (url: string) => axios.delete(url).then(responseBody),
+    get: (url: string) => axios.get(url).then(responseBody),
+    post: (url: string, body: object) => axios.post(url, body).then(responseBody),
+    put: (url: string, body: object) => axios.put(url, body).then(responseBody),
+    del: (url: string) => axios.delete(url).then(responseBody)
 };
 
 const Activities = {
