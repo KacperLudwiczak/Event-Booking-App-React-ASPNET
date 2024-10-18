49cd13e87e08373605628471f6600f00d03afa46
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-18 17:52:37 +0200

    Create ActivityForm

---
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -0,0 +1,79 @@
+import { ChangeEvent, useState } from "react";
+import { Button, ButtonGroup, Form, Segment } from "semantic-ui-react";
+
+const segmentStyles = {
+  padding: "25px",
+  borderRadius: "25px",
+  boxShadow: "0 6px 30px rgba(0, 0, 0, 0.1)",
+};
+
+export default function ActivityForm() {
+  const initialState = {
+    id: "",
+    title: "",
+    category: "",
+    description: "",
+    date: "",
+    city: "",
+    venue: "",
+  };
+  const [activity, setActivity] = useState(initialState);
+
+  function handleSubmit() {
+    // createOrEdit(activity);
+  }
+
+  function handleInputChange(
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) {
+    const { name, value } = event.target;
+    setActivity({ ...activity, [name]: value });
+  }
+
+  return (
+    <Segment clearing style={segmentStyles}>
+      <Form onSubmit={handleSubmit} autoComplete="off">
+        <Form.Input
+          placeholder="Title"
+          value={activity.title}
+          name="title"
+          onChange={handleInputChange}
+        />
+        <Form.TextArea
+          placeholder="Description"
+          value={activity.description}
+          name="description"
+          onChange={handleInputChange}
+        />
+        <Form.Input
+          placeholder="Category"
+          value={activity.category}
+          name="category"
+          onChange={handleInputChange}
+        />
+        <Form.Input
+          placeholder="Date"
+          value={activity.date}
+          name="date"
+          onChange={handleInputChange}
+        />
+        <Form.Input
+          placeholder="City"
+          value={activity.city}
+          name="city"
+          onChange={handleInputChange}
+        />
+        <Form.Input
+          placeholder="Venue"
+          value={activity.venue}
+          name="venue"
+          onChange={handleInputChange}
+        />
+        <ButtonGroup widths="2" style={{marginTop: "10px"}}>
+          <Button inverted color="blue" content="Submit" style={{ marginRight: "10px" }} />
+          <Button inverted color="orange"  content="Cancel" />
+        </ButtonGroup> 
+      </Form>
+    </Segment>
+  );
+}
