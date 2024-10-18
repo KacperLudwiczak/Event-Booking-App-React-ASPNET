63e26f5d0c2998f0992b18171c70677999634b12
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-18 17:13:34 +0200

    Styling NavBar

--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -4,11 +4,11 @@ export default function NavBar() {
     return (
         <Menu inverted secondary fixed='top'>
             <Container>
-                <Menu.Item header>
+                <Menu.Item header style={{ fontSize: '1.1em' }}>
                     <img src='/assets/calendar.png' alt='logo' style={{marginRight: 10}}/>
                     Reactivities
                 </Menu.Item>
-                <Menu.Item name='Activities' />
+                <Menu.Item name='Activities' style={{ fontSize: '1em' }}/>
                 <Menu.Item>
                     <Button inverted content='Create Activity' />
                 </Menu.Item>
