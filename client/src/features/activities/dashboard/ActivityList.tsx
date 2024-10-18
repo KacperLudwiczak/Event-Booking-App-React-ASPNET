83e0c89b04c65c6ec6e03b4401791a13162e9450
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-18 17:13:18 +0200

    Create ActivityList

---
+++ client/src/features/activities/dashboard/ActivityList.tsx
@@ -0,0 +1,40 @@
+import { Button, Item, Label, Segment } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
+
+const segmentStyles = {
+  padding: "25px",
+  borderRadius: "25px",
+  boxShadow: "0 6px 30px rgba(0, 0, 0, 0.1)",
+};
+
+interface Props {
+  activities: Activity[];
+}
+
+export default function ActivityList({ activities }: Props) {
+  return (
+    <Segment style={segmentStyles}>
+      <Item.Group divided>
+        {activities.map((activity) => (
+          <Item key={activity.id}>
+            <Item.Content>
+              <Item.Header as="a">{activity.title}</Item.Header>
+              <Item.Meta>{activity.date}</Item.Meta>
+              <Item.Description>
+                <div>{activity.description}</div>
+                <div>
+                  {activity.city}, {activity.venue}
+                </div>
+              </Item.Description>
+              <Item.Extra>
+                <Button inverted floated="right" content="View" color="blue" />
+                <Button inverted floated="right" content="Delete" color="red" />
+                <Label basic content={activity.category} />
+              </Item.Extra>
+            </Item.Content>
+          </Item>
+        ))}
+      </Item.Group>
+    </Segment>
+  );
+}
