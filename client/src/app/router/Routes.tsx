cc19eb55f27d42a8c01c1ac4f1601e04a0b7bba3
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-19 17:17:10 +0200

    Take HomePage outside of navbar

--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -3,14 +3,12 @@ import App from "../layout/App";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
 import ActivityForm from "../../features/activities/form/ActivityForm";
-import HomePage from "../../features/home/HomePage";
 
 export const routes: RouteObject[] = [
     {
         path: '/',
         element: <App />,
         children: [
-            {path: '', element: <HomePage />},
             {path: 'activities', element: <ActivityDashboard />},
             {path: 'activities/:id', element: <ActivityDetails />},
             {path: 'createActivity', element: <ActivityForm key='create' />},
--- client/src/features/home/HomePage.tsx
+++ client/src/features/home/HomePage.tsx
@@ -3,7 +3,7 @@ import { Container } from "semantic-ui-react";
 
 export default function HomePage() {
   return (
-    <Container style={{ marginTop: "7em" }}>
+    <Container style={{ marginTop: "3em" }}>
       <h1>Home page</h1>
       <h3>
         Go to <Link to="/activities">Activities</Link>
