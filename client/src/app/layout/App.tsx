763a4ea68776692c88052444876c18b6e8bb8403
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-19 15:29:23 +0200

    Add Outlet

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,10 +1,10 @@
 import { useEffect } from "react";
 import { Container } from "semantic-ui-react";
 import NavBar from "./NavBar";
-import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import LoadingComponent from "./LoadingComponent";
 import { useStore } from "../stores/store";
 import { observer } from "mobx-react-lite";
+import { Outlet } from "react-router-dom";
 
 function App() {
   const { activityStore } = useStore();
@@ -20,7 +20,7 @@ function App() {
     <>
       <NavBar />
       <Container style={{ marginTop: "7em" }}>
-        <ActivityDashboard />
+        <Outlet />
       </Container>
     </>
   );
