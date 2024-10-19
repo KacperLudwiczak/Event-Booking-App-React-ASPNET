466af7d616530b1c53f857f718a969a2c90451b7
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-19 13:48:57 +0200

    Styling loader

--- client/src/app/layout/LoadingComponent.tsx
+++ client/src/app/layout/LoadingComponent.tsx
@@ -1,14 +1,27 @@
-import {Dimmer, Loader} from "semantic-ui-react";
+import { Dimmer, Loader } from "semantic-ui-react";
+
+const dimmerStyles = {
+  background:
+    "linear-gradient(90deg, rgb(138, 192, 253) 0%, rgb(186, 215, 248) 51%, rgb(215, 231, 250) 100%)",
+};
+
+const loaderStyles = {
+    color:
+      "#fff",
+  };
 
 interface Props {
-    inverted?: boolean;
-    content: string;
+  inverted?: boolean;
+  content?: string;
 }
 
-export default function LoadingComponent({inverted = true, content = 'Loading...'}: Props) {
-    return (
-        <Dimmer active={true} inverted={inverted}>
-            <Loader size='big' content={content} />
-        </Dimmer>
-    )
+export default function LoadingComponent({
+  inverted = true,
+  content = "Loading...",
+}: Props) {
+  return (
+    <Dimmer active={true} inverted={inverted} style={dimmerStyles}>
+      <Loader size="massive" content={content} style={loaderStyles}/>
+    </Dimmer>
+  );
 }
