1ca9569972a082ad36ef8187f168d58085556f6e
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-21 10:49:21 +0200

    Delete comments

--- client/src/features/activities/dashboard/ActivityListItem.tsx
+++ client/src/features/activities/dashboard/ActivityListItem.tsx
@@ -66,40 +66,5 @@ export default function ActivityListItem({ activity }: Props) {
         </Item.Extra>
         </Item.Content>
         </Item>
-    // <Item key={activity.id}>
-    //   <Item.Content>
-    //     <Item.Header as="a">{activity.title}</Item.Header>
-    //     <Item.Description>
-    //       <div>{activity.description}</div>
-    //       <div>
-    //         {activity.city} | {activity.venue}
-    //       </div>
-    //     </Item.Description>
-    //     <Item.Extra>
-          // <Button
-          //   inverted
-          //   floated="right"
-          //   content="View"
-          //   color="blue"
-          //   as={Link}
-          //   to={`/activities/${activity.id}`}
-          // />
-          // <Button
-          //   inverted
-          //   name={activity.id}
-          //   floated="right"
-          //   content="Delete"
-          //   color="red"
-          //   loading={loading && target === activity.id}
-          //   onClick={(event) => handleDeleteActivity(event, activity.id)}
-          // />
-        //   <Label
-        //     basic
-        //     content={activity.category}
-        //     style={{ borderRadius: "20px", padding: "8px 12px" }}
-        //   />
-        // </Item.Extra>
-    //   </Item.Content>
-    // </Item>
   );
 }
