59891926df704c0f33ada88d1a14f339894b02ae
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-21 11:50:55 +0200

    Styling

--- client/src/features/activities/details/ActivityDetailedChat.tsx
+++ client/src/features/activities/details/ActivityDetailedChat.tsx
@@ -6,10 +6,12 @@ import {
   Form,
   Button,
   Item,
+  Image,
 } from "semantic-ui-react";
 
 const segmentStyles = {
   borderRadius: "25px",
+  backgroundColor: "#54c8ff",
   boxShadow: "0 6px 30px rgba(0, 0, 0, 0.1)",
   overflow: "hidden",
   padding: "15px",
@@ -17,48 +19,64 @@ const segmentStyles = {
   marginBottom: "25px",
 };
 
+const imageStyles = {
+    borderRadius: "50%",
+    width: "40px",
+    height: "40px",
+    marginRight: "10px",
+    marginBottom: "20px",
+  };
+
 function ActivityDetailedChat() {
   return (
-    <Item style={{marginBottom: "15px"}}>
-      <Segment
-        textAlign="center"
-        attached="top"
-        inverted
-        color="teal"
-        style={segmentStyles}
-      >
+    <Item style={{ marginBottom: "15px" }}>
+      <Segment textAlign="center" attached="top" inverted style={segmentStyles}>
         <Header>Chat about this event</Header>
       </Segment>
       <Item>
         <Comment.Group>
           <Comment>
-            <Comment.Avatar src="/assets/user.png" />
-            <Comment.Content>
-              <Comment.Author as="a">Matt</Comment.Author>
-              <Comment.Metadata>
-                <div>Today at 5:42PM</div>
-              </Comment.Metadata>
-              <Comment.Text>How artistic!</Comment.Text>
-              <Comment.Actions>
-                <Comment.Action>Reply</Comment.Action>
-              </Comment.Actions>
-            </Comment.Content>
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <Item>
+                <Image
+                  style={imageStyles}
+                  src="/assets/user.png"
+                />
+              </Item>
+              <Comment.Content>
+                <Comment.Author as="a">Matt</Comment.Author>
+                <Comment.Metadata>
+                  <div>Today at 5:42PM</div>
+                </Comment.Metadata>
+                <Comment.Text>How artistic!</Comment.Text>
+                <Comment.Actions>
+                  <Comment.Action>Reply</Comment.Action>
+                </Comment.Actions>
+              </Comment.Content>
+            </div>
           </Comment>
           <Comment>
-            <Comment.Avatar src="/assets/user.png" />
-            <Comment.Content>
-              <Comment.Author as="a">Joe Henderson</Comment.Author>
-              <Comment.Metadata>
-                <div>5 days ago</div>
-              </Comment.Metadata>
-              <Comment.Text>Dude, this is awesome. Thanks so much</Comment.Text>
-              <Comment.Actions>
-                <Comment.Action>Reply</Comment.Action>
-              </Comment.Actions>
-            </Comment.Content>
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <Item>
+                <Image
+                  style={imageStyles}
+                  src="/assets/user.png"
+                />
+              </Item>
+              <Comment.Content>
+                <Comment.Author as="a">Joe Henderson</Comment.Author>
+                <Comment.Metadata>
+                  <div>5 days ago</div>
+                </Comment.Metadata>
+                <Comment.Text>Dude, this is awesome. Thanks so much</Comment.Text>
+                <Comment.Actions>
+                  <Comment.Action>Reply</Comment.Action>
+                </Comment.Actions>
+              </Comment.Content>
+            </div>
           </Comment>
           <Form reply>
-            <Form.TextArea style={{borderRadius: "25px"}}/>
+            <Form.TextArea style={{ borderRadius: "25px" }} />
             <Button
               inverted
               content="Add Reply"
--- client/src/features/activities/details/ActivityDetailedInfo.tsx
+++ client/src/features/activities/details/ActivityDetailedInfo.tsx
@@ -1,48 +1,47 @@
-import { observer } from 'mobx-react-lite';
-import { Grid, Icon, Item} from 'semantic-ui-react'
-import {Activity} from "../../../app/models/activity";
+import { observer } from "mobx-react-lite";
+import { Grid, Icon, Item } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
 interface Props {
-    activity: Activity
+  activity: Activity;
 }
 
-function ActivityDetailedInfo({activity}: Props) {
-    return (
-        <Item style={{marginBottom: "25px"}}>
-        <Item.Content attached='top'>
-            <Grid>
-                <Grid.Column width={1}>
-                    <Icon size='large' color='teal' name='info'/>
-                </Grid.Column>
-                <Grid.Column width={15}>
-                    <p>{activity.description}</p>
-                </Grid.Column>
-            </Grid>
-        </Item.Content>
-        <Item.Content attached>
-            <Grid verticalAlign='middle'>
-                <Grid.Column width={1}>
-                    <Icon name='calendar' size='large' color='teal'/>
-                </Grid.Column>
-                <Grid.Column width={15}>
-        <span>
-          {activity.date}
-        </span>
-                </Grid.Column>
-            </Grid>
-        </Item.Content>
-        <Item.Content attached>
-            <Grid verticalAlign='middle'>
-                <Grid.Column width={1}>
-                    <Icon name='marker' size='large' color='teal'/>
-                </Grid.Column>
-                <Grid.Column width={11}>
-                    <span>{activity.venue} | {activity.city}</span>
-                </Grid.Column>
-            </Grid>
-        </Item.Content>
+function ActivityDetailedInfo({ activity }: Props) {
+  return (
+    <Item style={{ marginBottom: "25px" }}>
+      <Item.Content attached="top">
+        <Grid>
+          <Grid.Column width={1}>
+            <Icon size="large" name="info" style={{ color: "#54c8ff" }} />
+          </Grid.Column>
+          <Grid.Column width={15}>
+            <p>{activity.description}</p>
+          </Grid.Column>
+        </Grid>
+      </Item.Content>
+      <Item.Content attached>
+        <Grid verticalAlign="middle">
+          <Grid.Column width={1}>
+            <Icon name="calendar" size="large" style={{ color: "#54c8ff" }} />
+          </Grid.Column>
+          <Grid.Column width={15}>
+            <span>{activity.date}</span>
+          </Grid.Column>
+        </Grid>
+      </Item.Content>
+      <Item.Content attached>
+        <Grid verticalAlign="middle">
+          <Grid.Column width={1}>
+            <Icon name="marker" size="large" style={{ color: "#54c8ff" }} />
+          </Grid.Column>
+          <Grid.Column width={11}>
+            <span>
+              {activity.venue} | {activity.city}
+            </span>
+          </Grid.Column>
+        </Grid>
+      </Item.Content>
     </Item>
-
-    )
+  );
 }
 
 const ObservedActivityDetailedInfo = observer(ActivityDetailedInfo);
