150008d852a26d9faacf2e9392a4362e79f0ba1c
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-21 12:34:20 +0200

    Clean code

--- client/src/features/activities/dashboard/ActivityListItem.tsx
+++ client/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,52 +1,36 @@
 import { Link } from "react-router-dom";
-import { Item, Button, Icon} from "semantic-ui-react";
+import { Item, Button, Icon } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
-import { useStore } from "../../../app/stores/store";
-import { SyntheticEvent, useState } from "react";
 
 interface Props {
   activity: Activity;
 }
 
 export default function ActivityListItem({ activity }: Props) {
-  const { activityStore } = useStore();
-  const { deleteActivity, loading } = activityStore;
-
-  const [target, setTarget] = useState("");
-
-  function handleDeleteActivity(
-    event: SyntheticEvent<HTMLButtonElement>,
-    id: string
-  ) {
-    setTarget(event.currentTarget.name);
-    deleteActivity(id);
-  }
-
   return (
-    <Item style={{marginBottom: "5px"}}>
-    <Item.Content>
-      <Item.Group>
-        <Item>
-          <Item.Image size="tiny" circular src="/assets/user.png" />
-          <Item.Content style={{marginTop: "20px"}}>
-            <Item.Header as="a">{activity.title}</Item.Header>
-            <Item.Description>Hosted by Bob</Item.Description>
-          </Item.Content>
-        </Item>
-      </Item.Group>
+    <Item style={{ marginBottom: "5px" }}>
+      <Item.Content>
+        <Item.Group>
+          <Item>
+            <Item.Image size="tiny" circular src="/assets/user.png" />
+            <Item.Content style={{ marginTop: "20px" }}>
+              <Item.Header as="a">{activity.title}</Item.Header>
+              <Item.Description>Hosted by Bob</Item.Description>
+            </Item.Content>
+          </Item>
+        </Item.Group>
 
+        <span style={{ padding: "5px" }}>
+          <Icon name="clock" /> {activity.date}
+        </span>
+        <span style={{ padding: "5px" }}>
+          <Icon name="marker" /> {activity.venue}
+        </span>
 
-      <span  style={{padding: "5px"}}>
-         <Icon name="clock" /> {activity.date}
-      </span>
-      <span style={{padding: "5px"}}>
-      <Icon name="marker" /> {activity.venue}
-      </span>
-
-      <h5 style={{margin: "15px 10px"}}>Attendees go here</h5>
-      <h5 style={{margin: "15px 10px"}}>{activity.description}</h5>
-      <Item.Extra>
-      <Button
+        <h5 style={{ margin: "15px 10px" }}>Attendees go here</h5>
+        <h5 style={{ margin: "15px 10px" }}>{activity.description}</h5>
+        <Item.Extra>
+          <Button
             inverted
             floated="right"
             content="View"
@@ -54,17 +38,8 @@ export default function ActivityListItem({ activity }: Props) {
             as={Link}
             to={`/activities/${activity.id}`}
           />
-          <Button
-            inverted
-            name={activity.id}
-            floated="right"
-            content="Delete"
-            color="red"
-            loading={loading && target === activity.id}
-            onClick={(event) => handleDeleteActivity(event, activity.id)}
-          />
         </Item.Extra>
-        </Item.Content>
-        </Item>
+      </Item.Content>
+    </Item>
   );
 }
