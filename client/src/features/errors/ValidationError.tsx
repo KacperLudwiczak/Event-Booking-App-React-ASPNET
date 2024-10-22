79b72adb15e53cc1946d5374ddfe91330ba9909e
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-22 15:51:58 +0200

    Change ValidationError

--- client/src/features/errors/ValidationError.tsx
+++ client/src/features/errors/ValidationError.tsx
@@ -1,18 +1,25 @@
-import { Message } from "semantic-ui-react";
+import { Item, Message } from "semantic-ui-react";
+
+const itemStyles = {
+  padding: "25px",
+  color: "#fff",
+  fontSize: "20px",
+};
 
 interface Props {
-    errors: string[];
+  errors: string[];
 }
-export default function ValidationError({errors}: Props) {
-    return (
-        <Message error>
-            {errors && (
-                <Message.List>
-                    {errors.map((err: string, i) => (
-                        <Message.Item key={i}>{err}</Message.Item>
-                    ))}
-                </Message.List>
-            )}
-        </Message>
-    )
+
+export default function ValidationError({ errors }: Props) {
+  return (
+    <Item style={itemStyles}>
+      {errors && (
+        <Item>
+          {errors.map((err: string, i) => (
+            <Message.Item key={i}>{err}</Message.Item>
+          ))}
+        </Item>
+      )}
+    </Item>
+  );
 }
