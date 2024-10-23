46f24effd252f030fe1c5f87faf38be0f3c6864b
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-23 17:55:03 +0200

    Implement TokenService

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -1,11 +1,12 @@
 using System.Text;
+using API.Services;
 using Domain;
 using Persistence;
 namespace API.Extensions
 {
     public static class IdentityServiceExtensions
     {
-        public static IServiceCollection AddIdentityServices(this IServiceCollection services, 
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services,
             IConfiguration config)
         {
             services.AddIdentityCore<AppUser>(option =>
@@ -15,6 +16,7 @@ namespace API.Extensions
             })
             .AddEntityFrameworkStores<DataContext>();
             services.AddAuthentication();
+            services.AddScoped<TokenService>();
             return services;
         }
     }
