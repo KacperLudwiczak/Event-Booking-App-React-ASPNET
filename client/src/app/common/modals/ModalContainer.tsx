c213aa0b91fa4da8d775011084a1c6e3380ad5c2
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-24 13:37:15 +0200

    Styling modal

--- client/src/app/common/modals/ModalContainer.tsx
+++ client/src/app/common/modals/ModalContainer.tsx
@@ -7,8 +7,7 @@ const modalStyles = {
   padding: "25px",
   borderRadius: "25px",
   boxShadow: "0 6px 30px rgba(0, 0, 0, 0.1)",
-  width: "40%",
-  height: "60%"
+  width: "40%"
 };
 
 function ModalContainer() {
