d27673d2bee0228bee5ec814cce94923bed65320
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-24 13:37:57 +0200

    Styling login form

--- client/src/features/users/LoginForm.tsx
+++ client/src/features/users/LoginForm.tsx
@@ -8,56 +8,50 @@ function LoginForm() {
   const { userStore } = useStore();
 
   return (
-
-      <Formik
-        initialValues={{ email: "", password: "", error: null }}
-        onSubmit={(values, { setErrors }) =>
-          userStore
-            .login(values)
-            .catch(() => setErrors({ error: "Invalid email or password" }))
-        }
-      >
-        {({ handleSubmit, isSubmitting, errors }) => (
-          <Form
-            className="ui form"
-            onSubmit={handleSubmit}
-            autoComplete="off"
-            style={{ width: "100%" }}
-          >
-            <Header
-              content="Login to Reactivities"
-              textAlign="center"
-              style={{ fontSize: "40px", color: "#3ac0ff", padding: "30px" }}
+    <Formik
+      initialValues={{ email: "", password: "", error: null }}
+      onSubmit={(values, { setErrors }) =>
+        userStore
+          .login(values)
+          .catch(() => setErrors({ error: "Invalid email or password" }))
+      }
+    >
+      {({ handleSubmit, isSubmitting, errors }) => (
+        <Form
+          className="ui form"
+          onSubmit={handleSubmit}
+          autoComplete="off"
+          style={{ width: "100%" }}
+        >
+          <Header
+            content="Login to Reactivities"
+            textAlign="center"
+            style={{ fontSize: "40px", color: "#3ac0ff", padding: "30px" }}
+          />
+          <MyTextInput placeholder="Email" name="email" />
+          <MyTextInput placeholder="Password" name="password" type="password" />
+          <ErrorMessage
+            name="error"
+            render={() => (
+              <span style={{ color: "#9f3a38", fontSize: "12px" }}>
+                {errors.error}
+              </span>
+            )}
+          />
+          <div style={{ marginTop: "25px" }}>
+            <Button
+              loading={isSubmitting}
+              inverted
+              content="Login"
+              type="submit"
+              color="blue"
+              fluid
+              style={{ borderRadius: "25px" }}
             />
-            <MyTextInput placeholder="Email" name="email" />
-            <MyTextInput
-              placeholder="Password"
-              name="password"
-              type="password"
-            />
-            <ErrorMessage
-              name="error"
-              render={() => (
-                <span style={{ color: "#9f3a38", fontSize: "12px" }}>
-                  {errors.error}
-                </span>
-              )}
-            />
-            <div style={{ marginTop: "25px" }}>
-              <Button
-                loading={isSubmitting}
-                inverted
-                content="Login"
-                type="submit"
-                color="blue"
-                fluid
-                style={{ borderRadius: "25px" }}
-              />
-            </div>
-          </Form>
-        )}
-      </Formik>
- 
+          </div>
+        </Form>
+      )}
+    </Formik>
   );
 }
 
