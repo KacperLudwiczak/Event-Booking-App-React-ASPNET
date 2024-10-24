807bad8d8656d0b062403a4dc38918c46282b73d
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-24 11:32:14 +0200

    Add functions to commonStore

--- client/src/app/stores/commonStore.tsx
+++ client/src/app/stores/commonStore.tsx
@@ -2,12 +2,22 @@ import { makeAutoObservable } from "mobx";
 import { ServerError } from "../models/serverError";
 
 export default class CommonStore {
-    error: ServerError | null = null;
-    
-    constructor() {
-        makeAutoObservable(this);
-    }
-    setServerError(error: ServerError) {
-        this.error = error;
-    }
+  error: ServerError | null = null;
+  token: string | null | undefined = null;
+  appLoaded = false;
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  setServerError(error: ServerError) {
+    this.error = error;
+  }
+  setToken = (token: string | null) => {
+    if (token) localStorage.setItem("jwt", token);
+    this.token = token;
+  };
+  setAppLoaded = () => {
+    this.appLoaded = true;
+  };
 }
