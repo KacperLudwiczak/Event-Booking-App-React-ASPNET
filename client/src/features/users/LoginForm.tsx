5afc39bea229d0bec6488981c5e07262eb505adb
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-24 10:56:29 +0200

    Styling

--- client/src/features/users/LoginForm.tsx
+++ client/src/features/users/LoginForm.tsx
@@ -3,43 +3,57 @@ import { observer } from "mobx-react-lite";
 import { Button, Header, Label } from "semantic-ui-react";
 import MyTextInput from "../../app/common/form/MyTextInput";
 
+const containerStyles = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  height: "50vh",
+};
+
 function LoginForm() {
   return (
-    <Formik
-      initialValues={{ email: "", password: "", error: null }}
-      onSubmit={(values) => console.log(values)}
-    >
-      {({ handleSubmit, isSubmitting, errors }) => (
-        <Form className="ui form" onSubmit={handleSubmit} autoComplete="off">
-          <Header
-            as="h2"
-            content="Login to Reactivities"
-            color="teal"
-            textAlign="center"
-          />
-          <MyTextInput placeholder="Email" name="email" />
-          <MyTextInput placeholder="Password" name="password" />
-          <ErrorMessage
-            name="error"
-            render={() => (
-              <Label
-                style={{ marginBottom: 10 }}
-                basic
-                color="red"
-                content={errors.error}
-              />
-            )}
-          />
-          <Button
-            loading={isSubmitting}
-            positive
-            content="Login"
-            type="submit"
-            fluid
-          />
-        </Form>
-      )}
-    </Formik>
+    <div style={containerStyles}>
+      <Formik
+        initialValues={{ email: "", password: "", error: null }}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({ handleSubmit, isSubmitting, errors }) => (
+          <Form
+            className="ui form"
+            onSubmit={handleSubmit}
+            autoComplete="off"
+            style={{ width: "40%" }}
+          >
+            <Header
+              content="Login to Reactivities"
+              textAlign="center"
+              style={{ fontSize: "40px", color: "#fff", margin: "30px" }}
+            />
+            <MyTextInput placeholder="Email" name="email" />
+            <MyTextInput placeholder="Password" name="password" />
+            <ErrorMessage
+              name="error"
+              render={() => (
+                <Label
+                  style={{ marginBottom: 10 }}
+                  basic
+                  color="red"
+                  content={errors.error}
+                />
+              )}
+            />
+            <Button
+              loading={isSubmitting}
+              inverted
+              content="Login"
+              type="submit"
+              fluid
+              style={{ borderRadius: "25px" }}
+            />
+          </Form>
+        )}
+      </Formik>
+    </div>
   );
 }
 
