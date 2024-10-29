661459d1ecb63f9fc69f23f66e913f91f65e0b07
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-29 13:28:17 +0100

    Change List code to ProjectTo

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,5 +1,6 @@
 using Application.Core;
 using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -22,11 +23,9 @@ namespace Application.Activities
             public async Task<Result<List<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
             {
                 var activities = await _context.Activities
-                    .Include(attendees => attendees.Attendees)
-                    .ThenInclude(user => user.AppUser)
+                    .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider)
                     .ToListAsync(cancellationToken);
-                var activitiesToReturn = _mapper.Map<List<ActivityDto>>(activities);
-                return Result<List<ActivityDto>>.Success(activitiesToReturn);
+                return Result<List<ActivityDto>>.Success(activities);
             }
         }
     }
