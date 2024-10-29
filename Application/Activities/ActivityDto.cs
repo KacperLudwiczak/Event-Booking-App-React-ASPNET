1b7abf3d2aa46ab92fea2ccc0992857b09b06cb9
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-29 13:12:47 +0100

    Create Profile

--- Application/Activities/ActivityDto.cs
+++ Application/Activities/ActivityDto.cs
@@ -1,3 +1,5 @@
+using Application.Profiles;
+
 namespace Application.Activities
 {
     public class ActivityDto
@@ -11,6 +13,6 @@ namespace Application.Activities
         public string Venue { get; set; }
         public string HostUsername { get; set; }
         public bool IsCancelled { get; set; }
-
+        public ICollection<Profile> Attendees { get; set; }
     }
 }
