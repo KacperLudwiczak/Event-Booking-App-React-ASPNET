0bfebb3089d8a82de21d22bedfd91ccc0c52063e
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-29 13:21:41 +0100

    Add ForMember

--- Application/Core/ MappingProfiles.cs
+++ Application/Core/ MappingProfiles.cs
@@ -9,7 +9,9 @@ namespace Application.Core
         public MappingProfiles()
         {
             CreateMap<Activity, Activity>();
-            CreateMap<Activity, ActivityDto>();
+            CreateMap<Activity, ActivityDto>()
+                  .ForMember(destination => destination.HostUsername, options => options.MapFrom(source => source.Attendees
+                    .FirstOrDefault(item => item.IsHost).AppUser.UserName));
         }
     }
 }
