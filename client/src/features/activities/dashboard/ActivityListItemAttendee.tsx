518092ec74ab8f6dcde7767d95b66191caee5c35
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-31 13:20:26 +0100

    Add Popup

--- client/src/features/activities/dashboard/ActivityListItemAttendee.tsx
+++ client/src/features/activities/dashboard/ActivityListItemAttendee.tsx
@@ -1,7 +1,8 @@
-import { Image, List } from "semantic-ui-react";
+import { Image, List, Popup } from "semantic-ui-react";
 import { observer } from "mobx-react-lite";
 import { Link } from "react-router-dom";
 import { Profile } from "../../../app/models/profile";
+import ProfileCard from "../../profiles/ProfileCard";
 
 interface Props {
   attendees: Profile[];
@@ -11,17 +12,23 @@ function ActivityListItemAttendee({ attendees }: Props) {
   return (
     <List horizontal>
       {attendees.map((attendee) => (
-        <List.Item
+        <Popup
+          hoverable
           key={attendee.username}
-          as={Link}
-          to={`/profile/${attendee.username}`}
+          trigger={
+            <List.Item as={Link} to={`/profile/${attendee.username}`}>
+              <Image
+                size="mini"
+                circular
+                src={attendee.image || `/assets/user.png`}
+              />
+            </List.Item>
+          }
         >
-          <Image
-            size="mini"
-            circular
-            src={attendee.image || `/assets/user.png`}
-          />
-        </List.Item>
+          <Popup.Content>
+            <ProfileCard profile={attendee} />
+          </Popup.Content>
+        </Popup>
       ))}
     </List>
   );
