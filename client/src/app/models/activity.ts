c1884bbb5030da6cebfde3ae406347e22e8957a5
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-31 11:41:00 +0100

    Create ActivityFormValues

--- client/src/app/models/activity.ts
+++ client/src/app/models/activity.ts
@@ -11,7 +11,29 @@ export interface Activity {
   hostUsername?: string;
   isCancelled?: boolean;
   isGoing?: boolean;
-  isHost?: boolean
-  attendees?: Profile[]
+  isHost?: boolean;
+  attendees?: Profile[];
   host?: Profile;
+}
+
+export class ActivityFormValues {
+  id?: string = undefined;
+  title: string = "";
+  category: string = "";
+  description: string = "";
+  date: Date | null = null;
+  city: string = "";
+  venue: string = "";
+  
+  constructor(activity?: ActivityFormValues) {
+    if (activity) {
+      this.id = activity.id;
+      this.title = activity.title;
+      this.category = activity.category;
+      this.description = activity.description;
+      this.date = activity.date;
+      this.venue = activity.venue;
+      this.city = activity.city;
+    }
+  }
 }
