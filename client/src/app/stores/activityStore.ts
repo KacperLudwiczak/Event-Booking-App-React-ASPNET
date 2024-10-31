eef4bdc9ad0a0b2cbdf8d9e366a23b855d57f151
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-10-31 11:59:25 +0100

    Update updateActivity

--- client/src/app/stores/activityStore.ts
+++ client/src/app/stores/activityStore.ts
@@ -105,19 +105,18 @@ export default class ActivityStore {
     }
   };
 
-  updateActivity = async (activity: Activity) => {
-    this.loading = true;
+  updateActivity = async (activity: ActivityFormValues) => {
     try {
       await agent.Activities.update(activity);
       runInAction(() => {
-        this.activityRegistry.set(activity.id, activity);
-        this.selectedActivity = activity;
-        this.editMode = false;
-        this.loading = false;
+        if (activity.id) {
+          const updatedActivity = { ...this.getActivity(activity.id), ...activity };
+          this.activityRegistry.set(activity.id, updatedActivity as Activity);
+          this.selectedActivity = updatedActivity as Activity;
+      }
       });
     } catch (error) {
       console.log(error);
-      runInAction(() => (this.loading = false));
     }
   };
 
