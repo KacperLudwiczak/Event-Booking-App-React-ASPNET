57f1e973462e4a4bd4589ea81199ab8af7fee4f9
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-11-06 17:55:16 +0100

    Styling

--- client/src/features/profiles/ProfileContent.tsx
+++ client/src/features/profiles/ProfileContent.tsx
@@ -1,25 +1,43 @@
 import { observer } from "mobx-react-lite";
 import { Tab, TabPane } from "semantic-ui-react";
 
+const segmentStyles = {
+  padding: "25px",
+  borderRadius: "25px",
+  border: "none",
+  boxShadow: "0 6px 30px rgba(0, 0, 0, 0.1)",
+};
+
 function ProfileContent() {
   const panes = [
-    { menuItem: "About", render: () => <TabPane>About Content</TabPane> },
-    { menuItem: "Photos", render: () => <TabPane>Profile Photo</TabPane> },
-    { menuItem: "Events", render: () => <TabPane>Events Content</TabPane> },
+    {
+      menuItem: "About",
+      render: () => <TabPane style={segmentStyles} attached={false}>About Content</TabPane>,
+    },
+    {
+      menuItem: "Photos",
+      render: () => <TabPane style={segmentStyles}>Profile Photo</TabPane>,
+    },
+    {
+      menuItem: "Events",
+      render: () => <TabPane style={segmentStyles}>Events Content</TabPane>,
+    },
     {
       menuItem: "Followers",
-      render: () => <TabPane>Followers Content</TabPane>,
+      render: () => <TabPane style={segmentStyles}>Followers Content</TabPane>,
     },
     {
       menuItem: "Following",
-      render: () => <TabPane>Following Content</TabPane>,
+      render: () => <TabPane style={segmentStyles}>Following Content</TabPane>,
     },
   ];
   return (
     <Tab
-      menu={{ fluid: true, vertical: true }}
+      menu={{ secondary: true, vertical: true }}
+      menuItem={{color: "red"}}
       menuPosition="right"
       panes={panes}
+      style={{ width: "1200px" }}
     />
   );
 }
