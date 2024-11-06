c59758af719ce6d5fe0ba3f9d7adde7c776bf954
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-11-06 15:59:29 +0100

    Add ProfileContent

--- client/src/features/profiles/ProfilePage.tsx
+++ client/src/features/profiles/ProfilePage.tsx
@@ -1,12 +1,14 @@
 import { observer } from "mobx-react-lite";
 import { Grid } from "semantic-ui-react";
 import ProfileHeader from "./ProfileHeader";
+import ProfileContent from "./ProfileContent";
 
 function ProfilePage() {   
     return (
         <Grid>
           <Grid.Column width='16'>
                 <ProfileHeader />
+                <ProfileContent />
             </Grid.Column>
         </Grid>
     )
