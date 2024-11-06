1eb27dd4cf90821011aeee065cef7125dcb8d888
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-11-06 18:00:06 +0100

    Add Photo types

--- client/src/app/models/profile.ts
+++ client/src/app/models/profile.ts
@@ -1,19 +1,26 @@
 import { User } from "./user";
 
 export interface IProfile {
-    username: string;
-    displayName: string;
-    image?: string;
-    bio?: string;
+  username: string;
+  displayName: string;
+  image?: string;
+  bio?: string;
+  photos?: Photo[];
 }
 export class Profile implements IProfile {
-    constructor(user: User) {
-        this.username = user.username;
-        this.displayName = user.displayName;
-        this.image = user.image
-    }
-    username: string;
-    displayName: string;
-    image?: string;
-    bio?: string;
+  constructor(user: User) {
+    this.username = user.username;
+    this.displayName = user.displayName;
+    this.image = user.image;
+  }
+  username: string;
+  displayName: string;
+  image?: string;
+  bio?: string;
+}
+
+export interface Photo {
+  id: string;
+  url: string;
+  isMain: boolean;
 }
