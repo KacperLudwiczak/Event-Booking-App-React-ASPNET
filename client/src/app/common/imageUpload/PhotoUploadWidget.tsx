303cd20a50314cdb46049167f4cd846518554e63
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-11-12 16:32:19 +0100

    Styling

--- client/src/app/common/imageUpload/PhotoUploadWidget.tsx
+++ client/src/app/common/imageUpload/PhotoUploadWidget.tsx
@@ -12,16 +12,16 @@ function PhotoUploadWidget({ loading }: Props) {
       <Grid>
         <Grid.Row />
         <Grid.Column width={4}>
-          <Header color="teal" sub content="Step 1 - Add Photo" />
+          <Header content="Step 1 - Add Photo" style={{color: "#54c8ff"}}/>
           <PhotoUploadWidgetDropzone />
         </Grid.Column>
         <Grid.Column width={1} />
         <Grid.Column width={4}>
-          <Header sub color="teal" content="Step 2 - Resize image" />
+          <Header content="Step 2 - Resize"  style={{color: "#54c8ff"}}/>
         </Grid.Column>
         <Grid.Column width={1} />
         <Grid.Column width={4}>
-          <Header sub color="teal" content="Step 3 - Preview & Upload" />
+          <Header content="Step 3 - Upload"  style={{color: "#54c8ff"}}/>
           <div
             className="img-preview"
             style={{ minHeight: 200, overflow: "hidden" }}
