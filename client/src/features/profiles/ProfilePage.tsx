6a1d21f8acc5bb71006529640dc411173f4c77ea
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-11-12 15:01:56 +0100

    Add useParams, useStore and useEffect

--- client/src/features/profiles/ProfilePage.tsx
+++ client/src/features/profiles/ProfilePage.tsx
@@ -2,16 +2,32 @@ import { observer } from "mobx-react-lite";
 import { Grid } from "semantic-ui-react";
 import ProfileHeader from "./ProfileHeader";
 import ProfileContent from "./ProfileContent";
+import { useParams } from "react-router-dom";
+import { useStore } from "../../app/stores/store";
+import LoadingComponent from "../../app/layout/LoadingComponent";
+import { useEffect } from "react";
 
-function ProfilePage() {   
-    return (
-        <Grid>
-          <Grid.Column width='16'>
-                <ProfileHeader />
-                <ProfileContent />
-            </Grid.Column>
-        </Grid>
-    )
+function ProfilePage() {
+  const { username } = useParams();
+  const { profileStore } = useStore();
+  const { loadingProfile, loadProfile, profile } = profileStore;
+
+  useEffect(() => {
+    if (username) loadProfile(username);
+  }, [loadProfile, username]);
+  
+  if (loadingProfile)
+    return <LoadingComponent inverted content="Loading profile..." />;
+  if (!profile) return <h2>Problem loading profile</h2>;
+
+  return (
+    <Grid>
+      <Grid.Column width="16">
+        <ProfileHeader />
+        <ProfileContent />
+      </Grid.Column>
+    </Grid>
+  );
 }
 
 const ObservedProfilePage = observer(ProfilePage);
