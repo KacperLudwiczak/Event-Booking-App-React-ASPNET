99c205753874be62039035d0722a00f0b067f29e
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-11-12 17:06:12 +0100

    Styling

--- client/src/app/common/imageUpload/PhotoWidgetCropper.tsx
+++ client/src/app/common/imageUpload/PhotoWidgetCropper.tsx
@@ -10,7 +10,7 @@ export default function PhotoWidgetCropper({ imagePreview, setCropper }: Props) 
     return (
         <Cropper
             src={imagePreview}
-            style={{ height: 200, width: '100%' }}
+            style={{ height: 175, width: '100%' }}
             initialAspectRatio={1}
             aspectRatio={1}
             preview='.img-preview'
