ec0948a4f9afd8c4b9e6e3ec206adc61292636c8
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-11-20 13:58:57 +0100

    Add types to IProfile

--- client/src/app/models/profile.ts
+++ client/src/app/models/profile.ts
@@ -5,6 +5,9 @@ export interface IProfile {
   displayName: string;
   image?: string;
   bio?: string;
+  followersCount: number;
+  followingCount: number;
+  following: boolean;
   photos?: Photo[];
 }
 export class Profile implements IProfile {
