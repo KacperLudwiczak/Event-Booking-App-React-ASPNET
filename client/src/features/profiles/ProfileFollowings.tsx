d16c35df12c4471d784e1877ba19c48271e7ae7b
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-11-20 16:50:48 +0100

    Create ProfileFollowings

---
+++ client/src/features/profiles/ProfileFollowings.tsx
@@ -0,0 +1,47 @@
+import { Grid, Header, Card, TabPane } from "semantic-ui-react";
+import ProfileCard from "./ProfileCard";
+import { useStore } from "../../app/stores/store";
+import { observer } from "mobx-react-lite";
+
+const segmentStyles = {
+    border: "none",
+    borderRight: "1px solid #ddd",
+    boxShadow: "none",
+  };
+
+function ProfileFollowings() {
+  const { profileStore } = useStore();
+  const { profile, followings, loadingFollowings, activeTab } = profileStore;
+
+//   useEffect(() => {
+//     loadingFollowings("following");
+//   }, [loadingFollowings]);
+
+  return (
+    <TabPane loading={loadingFollowings} style={segmentStyles}>
+      <Grid>
+        <Grid.Column width="16">
+          <Header
+            floated="left"
+            icon="user"
+            content={
+              activeTab === 3
+                ? `People following ${profile!.displayName}`
+                : `People ${profile?.displayName} is following`
+            }
+          />
+        </Grid.Column>
+        <Grid.Column width="16">
+          <Card.Group itemsPerRow="5">
+            {followings.map((profile) => (
+              <ProfileCard key={profile.username} profile={profile} />
+            ))}
+          </Card.Group>
+        </Grid.Column>
+      </Grid>
+    </TabPane>
+  );
+}
+
+const ObservedProfileFollowings = observer(ProfileFollowings);
+export default ObservedProfileFollowings;
