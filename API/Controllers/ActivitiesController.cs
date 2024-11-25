a428c062fbab3adeb3ad822f582501545aef9efb
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-11-25 12:13:13 +0100

    Change name to HandlePagedResult

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -10,7 +10,7 @@ namespace API.Controllers
         [HttpGet] //api/activities
         public async Task<IActionResult> GetActivities([FromQuery] ActivityParams param)
         {
-            return HandleResult(await Mediator.Send(new List.Query { Params = param }));
+            return HandlePagedResult(await Mediator.Send(new List.Query { Params = param }));
         }
 
         [HttpGet("{id}")] //api/activities/id
