fef70a83bbf594b1cabcac8651e8988ff6c3f2e6
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-11-26 11:44:54 +0100

    Create PaginatedResult

--- client/src/app/models/pagination.ts
+++ client/src/app/models/pagination.ts
@@ -4,3 +4,12 @@ export interface Pagination {
     totalItems: number;
     totalPages: number;
 }
+
+export class PaginatedResult<T> {
+    data: T;
+    pagination: Pagination;
+    constructor(data: T, pagination: Pagination) {
+        this.data = data;
+        this.pagination = pagination;
+    }
+}
