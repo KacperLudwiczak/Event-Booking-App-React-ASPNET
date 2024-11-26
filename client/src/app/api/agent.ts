45fedd27758ad7b80d3714599c43494574a9ba03
KacperLudwiczak <casper.ludwiczak@gmail.com>
2024-11-26 11:54:00 +0100

    Add pagination

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -5,6 +5,7 @@ import { router } from "../router/Routes";
 import { store } from "../stores/store";
 import { User, UserFormValues } from "../models/user";
 import { Photo, Profile } from "../models/profile";
+import { PaginatedResult } from "../models/pagination";
 
 const sleep = (delay: number) => {
   return new Promise((resolve) => {
@@ -17,6 +18,11 @@ axios.defaults.baseURL = "http://localhost:5000/api";
 axios.interceptors.response.use(
   async (response) => {
     await sleep(1000);
+    const pagination = response.headers['pagination'];
+    if (pagination) {
+      response.data = new PaginatedResult(response.data, JSON.parse(pagination));
+      return response as AxiosResponse<PaginatedResult<Activity[]>>
+    }
     return response;
   },
   (error: AxiosError) => {
@@ -78,7 +84,7 @@ const requests = {
 };
 
 const Activities = {
-  list: () => requests.get<Activity[]>(`/activities`),
+  list: () => requests.get<PaginatedResult<Activity[]>>(`/activities`),
   details: (id: string) => requests.get<Activity>(`/activities/${id}`),
   create: (activity: ActivityFormValues) =>
     requests.post<void>(`/activities`, activity),
